59ecbfa3d2c1282eb5953c72ca466a606aac5c85
Safvan-tsy <safvanmanikulath@gmail.com>
2024-02-04 20:32:41 +0530

    feat:themes

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ThemeSwitch from "./ThemeSwitch";
+
+const NavBar = () => {
+  return (
+    <div>
+      NavBar
+      <ThemeSwitch />
+    </div>
+  );
+};
+
+export default NavBar;
