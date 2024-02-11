0d932f387c0830dba5e249240bce8e6d43843604
Safvan-tsy <safvanmanikulath@gmail.com>
2024-02-11 19:17:38 +0530

    login card

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginCard from "../components/ui/card/Login";
+
+const Login = () => {
+  return (
+    <div className="min-h-screen">
+      <LoginCard />
+    </div>
+  );
+};
+
+export default Login;
