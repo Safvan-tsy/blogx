2e6aee3db3d27ac780e00112dec7fbce3ca0ae73
Safvan-tsy <safvanmanikulath@gmail.com>
2024-03-02 17:10:05 +0530

    fix &apos; issue

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const token = req.headers.get("Authorization");
+  if(req.nextUrl.pathname.startsWith('/api/admin/login')){
+    //logic to check wheather admin account exists
+    return NextResponse.next()
+  }
+  if (token == null) return NextResponse.redirect(new URL("/login", req.url));
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/api/admin/:path*"],
+};
